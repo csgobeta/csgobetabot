7a41aef39fb0142582dffaf7a4f22c76c8d0d36d
abitmoony <summerbraum@gmail.com>
2021-02-15 23:41:54 +0000

    request reduce and multiple maps case

--- workshop_monitor.py
+++ workshop_monitor.py
@@ -16,24 +16,21 @@ url = 'https://steamcommunity.com/profiles/76561198082857351/myworkshopfiles/?ap
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
 
 def workshop_monitor():
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
+    currentTags = []
+    for tag in soup.find_all(class_="ugc"):
+        currentTags.append(tag.get('data-publishedfileid'))
     while True:
         try:
-            soupOne = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
-            currentTags = []
-            for tag in soupOne.find_all(class_="ugc"):
-                currentTags.append(tag.get('data-publishedfileid'))
-
-            time.sleep(60)
-
-            soupTwo = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
+            soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
             newTags = []
-            for tag in soupTwo.find_all(class_="ugc"):
+            for tag in soup.find_all(class_="ugc"):
                 newTags.append(tag.get('data-publishedfileid'))
 
             urlList, nameList = [], []
             if currentTags != newTags:
                 [i for i in currentTags if not i in newTags or newTags.remove(i)]
-                workshop = soupTwo.find("div", {"class": "workshopBrowseItems"})
+                workshop = soup.find("div", {"class": "workshopBrowseItems"})
                 for tag in newTags:
                     urlPath = workshop.find_all("a", attrs={"class": 'ugc', 'data-publishedfileid': tag})
                     for i in urlPath:
@@ -42,9 +39,21 @@ def workshop_monitor():
                         newName = i.find_parent('div').find('div', attrs={'class': 'workshopItemTitle'}).string.split()[0]
                         nameList.append(newName)
                 data = zip(urlList, nameList)
-                send_alert(data)
-
-            time.sleep(10)
+                if len(list(data)) < 2:
+                    data = zip(urlList, nameList)
+                    send_alert(data)
+                else:
+                    names = ', '.join(nameList)
+                    workshopUrl = 'https://steamcommunity.com/profiles/76561198082857351/myworkshopfiles/'
+                    bot = telebot.TeleBot(config.BOT_TOKEN)
+                    if not config.TEST_MODE:
+                        chatID = config.CSGOBETACHAT
+                    else:
+                        chatID = config.OWNER
+                    text = strings.notiNewMap_ru.format('ы', names, workshopUrl)
+                    bot.send_message(chatID, text, parse_mode='Markdown')
+            currentTags = newTags
+            time.sleep(60)
 
         except AttributeError:
             error_message = traceback.format_exc()
@@ -62,7 +71,6 @@ def send_alert(data):
     for x, y in data:
         text = strings.notiNewMap_ru.format(y, x)
         bot.send_message(chatID, text, parse_mode='Markdown')
-        time.sleep(10)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(process)d %(message)s')
