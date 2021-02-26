78dd0cd7f62e523bab4aa34d60a426d1ede14152
abitmoony <summerbraum@gmail.com>
2021-02-26 03:40:52 +0000

    Update user_rss.py

--- apps/user_rss.py
+++ apps/user_rss.py
@@ -5,13 +5,20 @@ sys.path.append(parentdir)
 
 import pandas as pd
 import telebot
+
 import time
+import logging
 import config
 
-with open(config.USER_MESSAGE_FILE_PATH, 'r') as f:
-    text = f.read()
-data = pd.read_csv(config.USER_DB_FILE_PATH)
-for userID in data['UserID']:
-    bot = telebot.TeleBot(config.BOT_TOKEN)
-    bot.send_message(userID, text, parse_mode='Markdown')
-    time.sleep(3)
+def send_messages():
+    data = pd.read_csv(config.USER_DB_FILE_PATH)
+    for Name, UserID in zip(data['Name'], data['UserID']):
+        with open(config.USER_MESSAGE_FILE_PATH, 'r') as f:
+            text = f.read().format(username = Name)
+        bot = telebot.TeleBot(config.BOT_TOKEN)
+        bot.send_message(UserID, text, parse_mode='Markdown')
+        time.sleep(3)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    send_messages()
