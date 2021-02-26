4129118e653e96e68a4b3d9b0c6fd9ede97cf06e
abitmoony <summerbraum@gmail.com>
2021-02-26 03:53:29 +0000

    lang separation

--- apps/user_rss.py
+++ apps/user_rss.py
@@ -8,16 +8,26 @@ import telebot
 
 import time
 import logging
+
 import config
+import strings
 
 def send_messages():
     data = pd.read_csv(config.USER_DB_FILE_PATH)
-    for Name, UserID in zip(data['Name'], data['UserID']):
-        with open(config.USER_MESSAGE_FILE_PATH, 'r') as f:
-            text = f.read().format(username = Name)
-        bot = telebot.TeleBot(config.BOT_TOKEN)
-        bot.send_message(UserID, text, parse_mode='Markdown')
-        time.sleep(3)
+    for name, userID, lang in zip(data['Name'], data['UserID'], data['Language']):
+        if lang in strings.CIS_lang_code:
+            with open(config.USER_RU_MESSAGE_FILE_PATH, 'r') as f:
+                text = f.read().format(username = name)
+            bot = telebot.TeleBot(config.BOT_TOKEN)
+            bot.send_message(userID, text, parse_mode='Markdown')
+            time.sleep(3)
+        else:
+            with open(config.USER_EN_MESSAGE_FILE_PATH, 'r') as f:
+                text = f.read().format(username = name)
+            bot = telebot.TeleBot(config.BOT_TOKEN)
+            bot.send_message(userID, text, parse_mode='Markdown')
+            time.sleep(3)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
